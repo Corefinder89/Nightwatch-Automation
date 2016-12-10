da40ecddb50bc2f1cf21767f41a139a6e9574c7a
SoumyajitBasu1988 <soumyajit.basu62@gmail.com>
2016-12-10 12:43:33 +0530

    Adding in changes for js code and nightwatch configuration

--- tests/Personal.js
+++ tests/Personal.js
@@ -1,8 +1,14 @@
 module.exports = {
-	'step one' : function(browser){
+	'Check for presence of body and title of Personal page' : function(browser){
 		browser
 		.url('http://stage.cgu.iagdev.net/personal')
-		.waitForElementVisible('body',1000)
-		.end();
+		.waitForElementVisible('body',2000)
+		.assert.title('Personal Insurance - Make A Claim Online | CGU Insurance')
+	},
+
+	'Check for text in the big tabs region' : function(browser){
+		browser
+		.assert.containsText('nav.big-tabs','OUR INSURANCE PRODUCTS')
+		.assert.containsText('nav.big-tabs','LEARN ABOUT INSURANCE')
 	}
 }
