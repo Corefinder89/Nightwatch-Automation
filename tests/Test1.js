81d3f510f4a3fbb4cb36fea0d1f486e11399f986
SoumyajitBasu1988 <soumyajit.basu62@gmail.com>
2016-12-11 13:30:18 +0530

    Adding in changes in the codebase for insurance section

--- tests/Test1.js
+++ tests/Test1.js
@@ -5,7 +5,7 @@ module.exports = {
 		},1000)
 	},
 
-	'Check for presence of body and title of Personal page' : function(browser){
+	'Title and Body section' : function(browser){
 		browser
 		 .useCss()
 		 .url('http://stage.cgu.iagdev.net/personal')
@@ -13,7 +13,7 @@ module.exports = {
 		 .assert.title('Personal Insurance - Make A Claim Online | CGU Insurance')
 	},
 
-	'Verify the urls in the header region' : function(browser){
+	'Header sectiom' : function(browser){
 		 browser.useCss()
 		 browser.expect.element('nav.site-nav ul').to.have.attribute('class').which.contains('menu')
 		 browser.expect.element('nav.site-nav ul li.has-megamenu:nth-child(1) a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/learn-about-insurance')
@@ -22,21 +22,26 @@ module.exports = {
 		 browser.expect.element('nav.site-nav ul li.has-megamenu:nth-child(4) a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/contact')
 	},
 
-	'Check for text in the big tabs region' : function(browser){
+	'Big tab section' : function(browser){
 		 browser.useXpath().assert.containsText("//nav[@class='big-tabs']/ul/li/a/h2[contains(text(),'Our insurance products')]",'OUR INSURANCE PRODUCTS')
 		 browser.useXpath().assert.containsText("//nav[@class='big-tabs']/ul/li/a/h2[contains(text(),'Learn about insurance')]",'LEARN ABOUT INSURANCE')
 	},
 
-	'Validate the components of the product section' : function(browser){
-		browser.useCss()
-		browser.expect.element('div.container div').to.be.visible
-		browser.expect.element('div.xs-accordian div:nth-child(1) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/car-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(2) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/home-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(3) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/boat-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(4) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/travel-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(5) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/residential-strata-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(5) div:nth-child(2) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/caravan-insurance')
-		browser.expect.element('div.xs-accordian div:nth-child(6) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/life-insurance')
+	'Product section' : function(browser){
+		browser.useCss().expect.element('section.business-landing-products div.container div').to.be.visible
+		browser.useXpath().assert.containsText("//div[@class='container']/div/div/h3[contains(text(),'Our most popular covers & products')]",'OUR MOST POPULAR COVERS & PRODUCTS')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(1) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/car-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(2) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/home-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(3) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/boat-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(4) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/travel-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(5) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/residential-strata-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(5) div:nth-child(2) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/caravan-insurance')
+		browser.useCss().expect.element('div.xs-accordian div:nth-child(6) div.backface a').to.have.attribute('href').which.contains('http://stage.cgu.iagdev.net/personal/products/life-insurance')
+	},
+
+	'Insurance section' : function(browser){
+		browser.useXpath().expect.element("//section[5]/div[@class='container']").to.be.visible
+		browser.useXpath().assert.containsText("//section[5]/div[@class='container']/div/div/h3[contains(text(),'What insurance is right for you?')]","WHAT INSURANCE IS RIGHT FOR YOU?")
 		browser.end()
 	}
 }
